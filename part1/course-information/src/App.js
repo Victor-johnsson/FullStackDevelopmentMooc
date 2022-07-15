3f0cf31b034cb77a01fe0be4359abdd45383ccee
Victor-johnsson <70903378+Victor-johnsson@users.noreply.github.com>
2022-07-15 19:44:41 +0200

    cleaned up after nodemodules deletion

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,69 +1,112 @@
-import { useState } from "react";
+import { useState } from 'react'
 
+
+
+/*
 const App = () => {
   const course = {
-    name: "Half Stack application development",
+    name: 'Half Stack application development',
     parts: [
       {
-        name: "Fundamentals of React",
-        exercises: 10,
+        name: 'Fundamentals of React',
+        exercises: 10
       },
       {
-        name: "Using props to pass data",
-        exercises: 7,
+        name: 'Using props to pass data',
+        exercises: 7
       },
       {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+ 
   return (
     <div>
       <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Content course = {course}/>
+      <Total course = {course} />
+      
+
     </div>
-  );
-};
+  )
+}
 
-const Header = (props) => {
-  console.log(props);
 
-  return <h1>{props.course["name"]}</h1>;
-};
+const Header =(props) =>{
+  console.log(props)
+
+  return(
+  
+    <h1>{props.course["name"]}</h1>
+  
+  )
+}
 
 const Content = (props) => {
-  console.log(props);
+  console.log(props )
 
-  const parts = props.course.parts;
+  const parts = props.course.parts
   return (
     <div>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
+      <Part part={parts[0]}  />
+      <Part part={parts[1]}  />
+      <Part part={parts[2]}  />
     </div>
-  );
-};
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
+  )
+}
+const Part = (props) =>{
+  return( <p>{props.part.name} {props.part.exercises}</p>)
+}
 
-const Total = (props) => {
-  console.log(props);
-  const parts = props.course.parts;
 
-  var i = 0;
-  parts.forEach((element) => {
-    i += element["exercises"];
+
+const Total = (props) =>{
+  console.log(props)
+  const parts = props.course.parts
+  
+  var i = 0
+  parts.forEach(element => {
+    i +=  element["exercises"]
   });
+  
+  
+  return(
+    <p>
+      {i}
+    </p>
+  )
 
-  return <p>{i}</p>;
-};
+}
+*/
 
-export default App;
+
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+export default App
